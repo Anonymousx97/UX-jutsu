92bd251be5c8589acf10b7889620398bda3d8e66
Ryuk <88324835+anonymousx97@users.noreply.github.com>
2022-10-03 14:57:48 +0530

    Try.

--- userge/plugins/help.py
+++ userge/plugins/help.py
@@ -656,22 +656,27 @@ if userge.has_bot:
                 me = await userge.get_me()
                 alive_info = Bot_Alive.alive_info(me)
                 buttons = Bot_Alive.alive_buttons()
-                if Config.ALIVE_MEDIA_TYPE == "photo":
-                    results.append(
-                        InlineQueryResultPhoto(
-                            photo_url=Config.NEW_ALIVE_MEDIA,
-                            caption=alive_info,
-                            reply_markup=buttons,
-                        )
-                    )
-                elif Config.ALIVE_MEDIA_TYPE == "gif":
-                    results.append(
-                        InlineQueryResultAnimation(
-                            animation_url=Config.NEW_ALIVE_MEDIA,
-                            caption=alive_info,
-                            reply_markup=buttons,
-                        )
-                    )
+                #if Config.ALIVE_MEDIA_TYPE == "photo":
+                    #results.append(
+                        #InlineQueryResultPhoto(
+                            #photo_url=Config.NEW_ALIVE_MEDIA,
+                            #caption=alive_info,
+                            #reply_markup=buttons,
+                        #)
+                    #)
+                #elif Config.ALIVE_MEDIA_TYPE == "gif":
+                    #results.append(
+                        #InlineQueryResultAnimation(
+                            #animation_url=Config.NEW_ALIVE_MEDIA,
+                            #caption=alive_info,
+                            #reply_markup=buttons,
+                        #)
+                    #)
+                results.append(title="Alive",
+                input_message_content=InputTextMessageContent(alive_info),
+                description="Alive",
+                reply_markup=InlineKeyboardMarkup(buttons),
+                )
 
             if string == "geass":
                 results.append(
