34ad3d6c6bb4aee259917646098742dc094f82c4
Ryuk <88324835+anonymousx97@users.noreply.github.com>
2022-12-08 22:47:04 +0530

    Update uselessv.py

--- userge/plugins/ryuk/uselessv.py
+++ userge/plugins/ryuk/uselessv.py
@@ -262,7 +262,7 @@ async def video_dl(userge, message: Message):
                     from pyrogram.errors import MediaEmpty, WebpageCurlFailed
                     from concurrent.futures import ThreadPoolExecutor
                     loop = asyncio.get_event_loop()
-                    i_dl = await loop.run_in_executor(ThreadPoolExecutor(),instadl(link))
+                    i_dl = await loop.run_in_executor(ThreadPoolExecutor(),instadl(url=link))
                     if i_dl == "not found":
                         await message.reply(
                             "Video download failed.\nLink not supported or private."
@@ -375,7 +375,7 @@ async def reddit_dl(userge, message: Message):
         await msg.delete()
 
 
-def instadl(url):
+def instadl(str:url):
     try:
         from selenium import webdriver
         from selenium.webdriver.common.by import By
