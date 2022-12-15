ffc7c5f156910fab2f996cd121e3f2979e0f9e76
Ryuk <90273333+anonymous-x97@users.noreply.github.com>
2022-12-15 12:55:06 +0530

    Update server.py

--- server.py
+++ server.py
@@ -10,4 +10,5 @@ async def read_root():
     return "healthy"
 
 if __name__=="__main__":
-    uvicorn.run(app,workers=1,host ="0.0.0.0" ,port=10000)
+    print("starting uvicorn")
+    uvicorn.run(app,workers=1,host ="0.0.0.0" ,port=10000,log_level="error")
