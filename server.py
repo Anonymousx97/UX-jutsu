e8c81ad4810b18c8c15ad5f4cd712f7acf6fc199
Ryuk <90273333+anonymous-x97@users.noreply.github.com>
2022-12-15 11:52:51 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,14 +2,12 @@ from fastapi import FastAPI
 import uvicorn
 import asyncio
 
+
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return "working"
-
-@app.get("/h")
-async def read_h():
     return "healthy"
 
-uvicorn.run(app,workers=1,host ="0.0.0.0" ,port=10000)
+if __name__=="__main__":
+    uvicorn.run(app,workers=1,host ="0.0.0.0" ,port=10000)
