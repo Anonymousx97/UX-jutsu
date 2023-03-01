f393d1716bf2985b49b6617a2746ba04d06dcd84
Billou <32905881+billouetaudrey@users.noreply.github.com>
2023-03-01 15:32:18 +0100

    Update alldebrid.py

--- userge/plugins/ryuk/alldebrid.py
+++ userge/plugins/ryuk/alldebrid.py
@@ -7,6 +7,8 @@ from userge import Message, userge
 
 # Your Alldbrid App token
 KEY = os.environ.get("DEBRID_TOKEN")
+WEBDAV = os.environ.get("DEBRID_WEBDAV")
+
 
 # Get response from api and return json or the error
 async def get_json(endpoint: str, query: dict):
@@ -98,7 +100,7 @@ async def torrents(message: Message):
             ret_val += f"""__{round(int(data.get("downloaded",0))/1000000)}__/"""
         ret_val += f"""__{round(int(data.get("size",0))/1000000)}__ mb"""
         ret_val += "\n\nSite: https://billouetaudrey.site/alldebrid/magnets/"
-        ret_val += "\n\nSite: https://myfiles.alldebrid.com/"DEBRID_WEBDAV"/magnets/"
+        ret_val += "\n\nSite: https://myfiles.alldebrid.com/"WEBDAV"/magnets/"
     else:
         ret_val = ""
         limit = 5
